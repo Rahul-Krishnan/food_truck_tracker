5f7f9fcff8875583798ccd65cf06f7bc9eabe211
Rahul Krishnan <rk2211@gmail.com>
2017-01-30 15:59:07 -0500

    updated readme token

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,9 +2,7 @@ class Appointment < ApplicationRecord
   belongs_to :truck
   belongs_to :location
   belongs_to :timeslot
-  
   validates :location, presence: true, uniqueness: true
   validates :timeslot, presence: true
   validates :truck, presence: true, uniqueness: { scope: :timeslot }
-  t.timestamps
 end
