b209a2c54e7d9ba77ef0f01ca6bfebef0ef95698
Rahul Krishnan <rk2211@gmail.com>
2017-02-02 16:02:45 -0500

    added factory, test, model fixes

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,5 @@
 class Location < ApplicationRecord
   has_many :appointments
   has_many :favorite_locations
-  validates :name, presence: true, uniqueness: true
   validates :address, presence: true
 end
--- spec/models/03_locations_spec.rb
+++ spec/models/03_locations_spec.rb
@@ -4,10 +4,6 @@ describe Location do
   let!(:location) { FactoryGirl.create(:location) }
 
   describe ".new" do
-    it "has a name" do
-      expect(location.name).to eq("Boston Public Library")
-    end
-
     it "has an address" do
       expect(location.address).to eq("700 Boylston St, Boston, MA 02116")
     end
