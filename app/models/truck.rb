267b044329c967bc85dbff1781174ba44fd59af6
Rahul Krishnan <rk2211@gmail.com>
2017-02-02 15:18:45 -0500

    Updated models, schema, tests, factories

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -2,5 +2,4 @@ class Truck < ApplicationRecord
   has_many :appointments
   has_many :favorite_trucks
   validates :name, presence: true, uniqueness: true
-  validates :category, presence: true
 end
--- spec/factories/locations.rb
+++ spec/factories/locations.rb
@@ -1,7 +1,6 @@
 FactoryGirl.define do
   factory :location do
 
-    name "Boston Public Library"
     address "700 Boylston St, Boston, MA 02116"
   end
 end
