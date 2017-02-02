3ac60f3291cbd672103fc65d3bf32fa40e2d3e3a
Rahul Krishnan <rk2211@gmail.com>
2017-02-02 10:53:56 -0500

    scraper method now outputs data array

--- app/jobs/scrape_job.rb
+++ app/jobs/scrape_job.rb
@@ -7,7 +7,6 @@ class ScrapeJob < ApplicationJob
 
   require 'httparty'
   require 'Nokogiri'
-  require 'open-uri'
 
   def real_scrape
     page = Nokogiri::HTML(HTTParty.get("http://www.cityofboston.gov/foodtrucks/schedule-app-min.asp").body)
@@ -15,9 +14,13 @@ class ScrapeJob < ApplicationJob
     dayData=page.css("td .dow")
     mealData=page.css("td .tod")
     locationData=page.css("td .loc")
-    appointmentData={}
+    appointmentData=[]
     truckData.each_with_index do |truck, n|
-      appointment[n] = Appointment.new{ location: locationData[n].text, timeslot: { day: dayData[n].text, time: mealData[n].text }, truck: truck.text }
+      longLocation = locationData[n].text
+      halfLocation = longLocation[/;(.+)/]
+      realLocation = halfLocation.last(halfLocation.length - 1)
+      appointmentData << { location: realLocation, timeslot: { day: dayData[n].text, time: mealData[n].text }, truck: truck.text }
     end
+    binding.pry
   end
 end
