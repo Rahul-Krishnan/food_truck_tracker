063f4448d8c9bc2405c9612a9257f88325558b77
Rahul Krishnan <rk2211@gmail.com>
2017-02-02 09:17:17 -0500

    removed name column from locations

--- app/jobs/scrape_job.rb
+++ app/jobs/scrape_job.rb
@@ -4,4 +4,20 @@ class ScrapeJob < ApplicationJob
   def scrape
     puts "whatup"
   end
+
+  require 'httparty'
+  require 'Nokogiri'
+  require 'open-uri'
+
+  def real_scrape
+    page = Nokogiri::HTML(HTTParty.get("http://www.cityofboston.gov/foodtrucks/schedule-app-min.asp").body)
+    truckData=page.css("td .com")
+    dayData=page.css("td .dow")
+    mealData=page.css("td .tod")
+    locationData=page.css("td .loc")
+    appointmentData={}
+    truckData.each_with_index do |truck, n|
+      appointment[n] = Appointment.new{ location: locationData[n].text, timeslot: { day: dayData[n].text, time: mealData[n].text }, truck: truck.text }
+    end
+  end
 end
---
+++ db/migrate/20170202140851_remove_name_from_location.rb
@@ -0,0 +1,8 @@
+class RemoveNameFromLocation < ActiveRecord::Migration[5.0]
+  def up
+    remove_column :locations, :name
+  end
+  def down
+    add_column :locations, :name, :string, null: false
+  end
+end
