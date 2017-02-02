f60ad5112190bc89cbb1352fe1fbd4c46b6f9392
Rahul Krishnan <rk2211@gmail.com>
2017-02-02 12:14:31 -0500

    updated styling, removed category column from trucks

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,6 +2,5 @@ class Location < ApplicationRecord
   has_many :appointments
   has_many :favorite_locations
   has_many :users, through: :favorite_locations
-  validates :name, presence: true, uniqueness: true
   validates :address, presence: true
 end
--- app/models/truck.rb
+++ app/models/truck.rb
@@ -3,5 +3,4 @@ class Truck < ApplicationRecord
   has_many :favorite_trucks
   has_many :users, through: :favorite_trucks
   validates :name, presence: true, uniqueness: true
-  validates :category, presence: true
 end
