b2fad51497377158ada301eabf4a86fb27906a11
Rahul Krishnan <rk2211@gmail.com>
2017-02-02 10:56:28 -0500

    hound fixes

--- app/jobs/scrape_job.rb
+++ app/jobs/scrape_job.rb
@@ -10,17 +10,16 @@ class ScrapeJob < ApplicationJob
 
   def real_scrape
     page = Nokogiri::HTML(HTTParty.get("http://www.cityofboston.gov/foodtrucks/schedule-app-min.asp").body)
-    truckData=page.css("td .com")
-    dayData=page.css("td .dow")
-    mealData=page.css("td .tod")
-    locationData=page.css("td .loc")
-    appointmentData=[]
-    truckData.each_with_index do |truck, n|
-      longLocation = locationData[n].text
-      halfLocation = longLocation[/;(.+)/]
-      realLocation = halfLocation.last(halfLocation.length - 1)
-      appointmentData << { location: realLocation, timeslot: { day: dayData[n].text, time: mealData[n].text }, truck: truck.text }
+    truck_data=page.css("td .com")
+    day_data=page.css("td .dow")
+    meal_data=page.css("td .tod")
+    location_data=page.css("td .loc")
+    appointment_data=[]
+    truck_data.each_with_index do |truck, n|
+      long_location = location_data[n].text
+      half_location = longLocation[/;(.+)/]
+      real_location = half_location.last(half_location.length - 1)
+      appointment_data << { location: real_location, timeslot: { day: day_data[n].text, time: meal_data[n].text }, truck: truck.text }
     end
-    binding.pry
   end
 end
