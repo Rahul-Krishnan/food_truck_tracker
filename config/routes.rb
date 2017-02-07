e8c00fc41dd7e3b5fd41f2a18545e2b5a97a49fb
Rahul Krishnan <rk2211@gmail.com>
2017-02-07 15:04:53 -0500

    made front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'appointments#index'
+  root 'home#index'
 
   resources :trucks, only: [:index, :show]
   resources :locations, only: [:index]
   resources :appointments, only: [:index]
   resources :favorite_trucks
+  resources :home, only: [:index]
 
   namespace :api do
     namespace :v1 do
