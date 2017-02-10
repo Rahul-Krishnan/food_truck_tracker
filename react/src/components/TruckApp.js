d3e32aec22a22b4b5d4e844d8f24e2d6ecf5f061
Rahul Krishnan <rk2211@gmail.com>
2017-02-10 14:05:45 -0500

    Added initial data list and data filter

--- react/src/components/TruckApp.js
+++ react/src/components/TruckApp.js
@@ -310,6 +310,7 @@ class TruckApp extends Component {
     .then(body => {
       let appointments = body;
       this.setState({ filteredDataList: appointments });
+      debugger;
     })
     .catch(error => console.error(`Error in fetch: ${error.message}`));
   }
