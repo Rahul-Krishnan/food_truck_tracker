967265443a0998cc6c92f2287e14c435deab4f7b
Rahul Krishnan <rk2211@gmail.com>
2017-02-13 16:38:33 -0500

    spacing fixed

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -9,12 +9,12 @@ class Api::V1::AppointmentsController < ApplicationController
       @timeslot = Timeslot.find(appointment[:timeslot_id])
       @appointments << { "truck": @truck.name, "location": @location.address, "day": @timeslot.day, "time": @timeslot.time }
     end
-    render json: {appointments: @appointments}
+    render json: { appointments: @appointments }
   end
 
   def show
     @appointment = Appointment.find(params[:id])
-    render json: {appointment: @appointment}
+    render json: { appointment: @appointment }
   end
 
 end
