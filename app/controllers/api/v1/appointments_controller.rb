d21df93bc97147a1b9e80dd6dc9f5153750df925
Rahul Krishnan <rk2211@gmail.com>
2017-02-13 16:34:22 -0500

    fixed fetch request for all data in table

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::AppointmentsController < ApplicationController
 
   def index
-    @appointments = Appointment.all
+    @appointments = []
+    @appointment_ids = Appointment.all
+    @appointment_ids.each do |appointment|
+      @truck = Truck.find(appointment[:truck_id])
+      @location = Location.find(appointment[:location_id])
+      @timeslot = Timeslot.find(appointment[:timeslot_id])
+      @appointments << { "truck": @truck.name, "location": @location.address, "day": @timeslot.day, "time": @timeslot.time }
+    end
     render json: {appointments: @appointments}
   end
 
