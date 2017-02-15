3c681b19db21ac4344ec1c5ce4be064e44ec293e
Rahul Krishnan <rk2211@gmail.com>
2017-02-14 20:43:12 -0500

    removed some of factory

--- spec/factories/appointments.rb
+++ spec/factories/appointments.rb
@@ -1,8 +1,5 @@
 FactoryGirl.define do
   factory :appointment do
-    :location = FactoryGirl.create(:location)
-    :timeslot = FactoryGirl.create(:timeslot)
-    :truck = FactoryGirl.create(:truck)
     location :location
     timeslot :timeslot
     truck :truck
--- spec/models/04_appointments_spec.rb
+++ spec/models/04_appointments_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Appointment do
   let!(:appointment) { FactoryGirl.create(:appointment) }
 
-  describe ".new" do
+  xdescribe ".new" do
     it "has an address" do
       expect(location.address).to eq("700 Boylston St, Boston, MA 02116")
     end
