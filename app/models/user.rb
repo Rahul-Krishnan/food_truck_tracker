6f17c5ac29fe0684569e565e7bade61f13328362
Rahul Krishnan <rk2211@gmail.com>
2017-02-17 19:14:28 -0500

    added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,10 @@ class User < ApplicationRecord
   has_many :favorite_timeslots
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :name, presence: true
+  validates :password, presence: true
+  validates :password_confirmation presence: true
+  validates :email, presence: true, uniqueness: true
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
 end
