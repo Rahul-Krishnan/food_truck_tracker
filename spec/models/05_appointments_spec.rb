d163ad6b84d1749bba03d6b14551146ef1e62c91
Rahul Krishnan <rk2211@gmail.com>
2017-02-21 10:22:53 -0500

    add appointment test

--- spec/models/05_appointments_spec.rb
+++ spec/models/05_appointments_spec.rb
@@ -7,7 +7,7 @@ describe Appointment do
   let!(:user) { FactoryGirl.create(:user) }
   let!(:appointment) { FactoryGirl.create(:appointment) }
 
-  xdescribe ".new" do
+  describe ".new" do
     it "has an address" do
       expect(location.address).to eq("700 Boylston St, Boston, MA 02116")
     end
